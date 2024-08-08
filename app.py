0444aedbafa29c978eca0769a9c30649136a6ea1
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-08-08 11:38:39 +0700

    Adding Website View

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-import random
+from flask import Flask, render_template, request
 
-angkaRahasia = random.randint(1, 100)
-angkaTebakan = None
+app = Flask(__name__)
+
+# Initialize global variables
+angka_rahasia = None
 percobaan = 0
 
-while angkaTebakan != angkaRahasia:
-    angkaTebakan = int(input("Masukkan angka tebakan 1-100: "))
-    percobaan += 1  # Tambahkan perhitungan percobaan di sini
+def reset_game():
+    global angka_rahasia, percobaan
+    angka_rahasia = random.randint(1, 100)
+    percobaan = 0
 
-    if angkaTebakan < angkaRahasia:
-        print("Angka tebakan terlalu kecil!")
-    elif angkaTebakan > angkaRahasia:
-        print("Angka tebakan terlalu besar!")
-    else:
-        print(f'Selamat, Anda berhasil menebak angka dalam {percobaan} percobaan!')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global percobaan
+    message = ''
+    if request.method == 'POST':
+        angka_tebakan = int(request.form['angka_tebakan'])
+        percobaan += 1
+        if angka_tebakan < angka_rahasia:
+            message = "Angka tebakan terlalu kecil!"
+        elif angka_tebakan > angka_rahasia:
+            message = "Angka tebakan terlalu besar!"
+        else:
+            message = f'Selamat, Anda berhasil menebak angka dalam {percobaan} percobaan!'
+            reset_game()
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    import random
+    reset_game()
+    app.run(debug=True)
