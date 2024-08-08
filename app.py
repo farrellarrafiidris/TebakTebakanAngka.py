5341c52157ca6fd0e906fcf5e2579cf3399aa93d
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-08-08 11:00:41 +0700

    Adding Small Things

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import random
 
-angkaRahasia =  random.randint(1,100);
-angkaTebakan = None;
+angkaRahasia = random.randint(1, 100)
+angkaTebakan = None
+percobaan = 0
 
 while angkaTebakan != angkaRahasia:
     angkaTebakan = int(input("Masukkan angka tebakan 1-100: "))
-    
+    percobaan += 1  # Tambahkan perhitungan percobaan di sini
+
     if angkaTebakan < angkaRahasia:
         print("Angka tebakan terlalu kecil!")
     elif angkaTebakan > angkaRahasia:
         print("Angka tebakan terlalu besar!")
     else:
-        print("Selamat, anda berhasil menebak angka!")
-        break;
+        print(f'Selamat, Anda berhasil menebak angka dalam {percobaan} percobaan!')
